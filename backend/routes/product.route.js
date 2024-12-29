5b91d98b6d4ecb40ead6b07672ead8995b640d33
Suwan-Aiken <suwan.sankaja@gmail.com>
2024-12-29 07:46:05 +0530

    added product routes

---
+++ backend/routes/product.route.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import Product from '../models/product.model.js';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const product = req.body;
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({success:false, message: 'All fields are required' });
+    }
+
+    const newProduct = new Product(product);
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true, message: 'Product created successfully', data: newProduct});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success:false, message: 'Error in creating product'});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    console.log(id);
+    try {
+        await Product.findByIdAndDelete(id);
+        res.json({success:true, message: 'Product deleted successfully'});
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({success:false, message: 'Product not found'});
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.json({success:true, data: products});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success:false, message: 'Error in fetching products'});
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = req.body;
+    try {
+        await Product.findByIdAndUpdate(id, product, {new: true});
+        res.json({success:true, message: 'Product updated successfully'});
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({success:false, message: 'Product not found'});
+    }});
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
@@ -9,11 +10,12 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+
 app.listen(PORT, () => {
     connectDB();
     console.log('Server is running on http://localhost:' + PORT);
 });
 
-app.get('/products', (req, res) => {
-
-});
